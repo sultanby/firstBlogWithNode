4cf2dab3060e0668e5d90a7136f2bcf8244dc619
sultanby <jemjasott@gmail.com>
2021-02-18 17:19:44 +0100

    update

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const express = require('express')
 const path = require('path')
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true, useUnifiedTopology: true })
+const BlogPost = require('./models/BlogPost.js')
+
 const app = new express()
 const ejs = require('ejs')
 app.set('view engine','ejs')
 app.use(express.static('public'))
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+const fileUpload = require('express-fileupload')
+app.use(fileUpload())
+
 app.listen(4000, ()=>{
     console.log('App listening on port 4000')
 })
 
-app.get('/',(req,res)=>{
-    res.render('index')
+app.get('/',async (req,res)=>{
+    const blogposts = await BlogPost.find({})
+    console.log(blogposts)
+    res.render('index',{
+        blogposts
+    });
 })
 
 app.get('/about',(req,res)=>{
@@ -22,6 +37,26 @@ app.get('/contact',(req,res)=>{
     res.render('contact')
 })
 
-app.get('/post',(req,res)=>{
-    res.render('post')
+app.get('/post/:id',async (req,res)=>{
+    console.log(req.params)
+    const blogpost = await BlogPost.findById(req.params.id)
+    res.render('post',{
+        blogpost
+    })
 })
+
+app.get('/posts/new',(req,res)=>{
+    res.render('create')
+})
+
+app.post('/posts/store', (req,res)=>{
+    let image = req.files.image;
+    image.mv(path.resolve(__dirname,'public/img',image.name),async(error)=>{
+        await BlogPost.create({
+            ...req.body,
+            image: '/img/' + image.name
+        })
+        res.redirect('/')
+    })
+})
+
---
+++ test.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const BlogPost = require('./models/BlogPost')
+mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true});
+BlogPost.create({
+    title: 'The Mythbuster’s Guide to Saving Money on Energy Bills',
+    body: 'If you have been here a long time, you might remember when I went on ITV Tonight to dispense a masterclass in saving money on energy bills. Energy-saving is one of my favourite money topics, because once you get past the boring bullet-point lists, a whole new world of thrifty nerdery opens up. You know those bullet-point lists. You start spotting them everything at this time of year. They go like this:'
+}, (error, blogpost) =>{
+    console.log(error,blogpost)
+})
+BlogPost.find({}, (error, blogspot) =>{
+    console.log(error,blogspot)
+})
+BlogPost.find({
+    title:'The Mythbuster’s Guide to Saving Money on Energy Bills'
+}, (error, blogspot) =>{
+    console.log(error,blogspot)
+})
+
+let id = "602e4c876ce11e07d7a43e4a";
+BlogPost.findByIdAndUpdate(id,{ title:'Updated title'
+}, (error, blogspot) =>{
+    console.log(error,blogspot)
+})
+
+BlogPost.findByIdAndDelete(id, (error, blogspot) =>{
+    console.log(error,blogspot)
+})
