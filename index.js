54e535769a02b0191f3e75224987adc7c7727a29
sultanby <jemjasott@gmail.com>
2021-02-19 15:45:02 +0100

    Controller added

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const path = require('path')
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true, useUnifiedTopology: true })
-const BlogPost = require('./models/BlogPost.js')
+mongoose.connect('mongodb://localhost/my_database', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 const app = new express()
 const ejs = require('ejs')
@@ -17,46 +18,18 @@ app.use(bodyParser.urlencoded({extended:true}))
 const fileUpload = require('express-fileupload')
 app.use(fileUpload())
 
+const validateMiddleware = require("./middleware/validationMiddleware");
+
+const newPostController = require('./controllers/newPost')
+const homeController = require('./controllers/home')
+const storePostController = require('./controllers/storePost')
+const getPostController = require('./controllers/getPost')
+
 app.listen(4000, ()=>{
     console.log('App listening on port 4000')
 })
 
-app.get('/',async (req,res)=>{
-    const blogposts = await BlogPost.find({})
-    console.log(blogposts)
-    res.render('index',{
-        blogposts
-    });
-})
-
-app.get('/about',(req,res)=>{
-    res.render('about')
-})
-
-app.get('/contact',(req,res)=>{
-    res.render('contact')
-})
-
-app.get('/post/:id',async (req,res)=>{
-    console.log(req.params)
-    const blogpost = await BlogPost.findById(req.params.id)
-    res.render('post',{
-        blogpost
-    })
-})
-
-app.get('/posts/new',(req,res)=>{
-    res.render('create')
-})
-
-app.post('/posts/store', (req,res)=>{
-    let image = req.files.image;
-    image.mv(path.resolve(__dirname,'public/img',image.name),async(error)=>{
-        await BlogPost.create({
-            ...req.body,
-            image: '/img/' + image.name
-        })
-        res.redirect('/')
-    })
-})
-
+app.get('/',homeController)
+app.get('/post/:id',getPostController)
+app.post('/posts/store', validateMiddleware, storePostController)
+app.get('/posts/new',newPostController)
